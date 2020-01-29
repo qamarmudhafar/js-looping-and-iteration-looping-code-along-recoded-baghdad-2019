533ffaba433263739097eed8671596e3eb0e1e28
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-29 13:05:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ['Lisa', 'Kaitlin', 'Jan'];
+ 
+function writeCards(gifts) {
+  let i = 0; // the initialization moved OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log();
+    gifts[i]=`Thank you, ${gifts[i]}, for the wonderful surprise gift!`;
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+ 
+  return gifts;
+}
+ 
+writeCards(gifts);
+
+
+function countDown(number) {
+ for(let i =number;i>=0;i--){
+    console.log(number);  
+    number-=1;
+ }
+ 
+  }
+
+
+
+countDown(5);
